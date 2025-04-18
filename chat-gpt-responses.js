61b9de78f64a22e69bdfeaa1865f97c9638587c1
guidone <guido.bellomo@gmail.com>
2025-04-18 10:49:40 +0200

    fix previous id in function output

--- chat-gpt-responses.js
+++ chat-gpt-responses.js
@@ -7,7 +7,18 @@ const resolver = require('./helpers/resolver');
 const updateTokens = require('./helpers/update-tokens');
 const GPTContext = require('./helpers/gpt-context');
 
-// DOC: UI element for dialog https://nodered.org/docs/creating-nodes/edit-dialog
+// DOCS:
+// UI element for dialog
+// https://nodered.org/docs/creating-nodes/edit-dialog
+//
+// OpenAPI function calls
+// https://platform.openai.com/docs/guides/function-calling?api-mode=responses&strict-mode=enabled#function-calling-steps
+//
+// OpenAPI responses API
+// https://platform.openai.com/docs/api-reference/responses/create
+//
+// OpenAPI conversation state
+// https://platform.openai.com/docs/guides/conversation-state?api-mode=responses
 
 module.exports = function(RED) {
   function ChatGPTResponses(config) {
@@ -52,26 +63,35 @@ module.exports = function(RED) {
         node.warn(`Was not possible to extract a session id from msg payload, a session will not be created it will not be possible to follow up messages with ChatGPT`);
       }
 
+      const session = await context.getSession(sessionId);
+      console.log('current session', session);
+
+      // prepare the call to openAI
+      let response;
       if (isFunctionResponse(msg)) {
+
         // HAMDLE MESSAGE RESPONSE
         console.log('answering to ', msg['chatgpt-function-call']);
+        console.log('');
 
         const gptRequest = {
           ...promptDesign,
           input: [
             {
-              type: "function_call_output",
+              type: 'function_call_output',
               call_id: msg['chatgpt-function-call'].call_id,
-              output: msg.payload ? msg.payload.toString() : null
+              output: msg.payload ? msg.payload.toString() : ''
             }
           ],
           previous_response_id: msg['chatgpt-function-call'].previousId,
           // override store flag
-          store: true
+          store: true,
+          tool_choice: 'auto'
         };
 
+        console.log('Func gptRequest', gptRequest);
+
         // execute call
-        let response;
         try {
           response = await openai.responses.create(gptRequest);
         } catch(e) {
@@ -79,16 +99,7 @@ module.exports = function(RED) {
           return;
         }
 
-        // update status
-        updateTokens(node, response)
-
-        send(processOutputs(response.output, promptDesign, msg, response));
-        done();
-
       } else {
-        const session = await context.getSession(sessionId);
-
-        console.log('current session', session);
 
         const gptRequest = {
           ...promptDesign,
@@ -105,7 +116,8 @@ module.exports = function(RED) {
             }
           ],
           // override store flag
-          store: true
+          store: true,
+          tool_choice: 'auto'
         };
 
         // set previous
@@ -113,35 +125,35 @@ module.exports = function(RED) {
           gptRequest.previous_response_id = session.previousId;
         }
 
+        console.log('Bare gptRequest', gptRequest);
         // execute call
-        let response;
         try {
           response = await openai.responses.create(gptRequest);
         } catch(e) {
           done(e);
           return;
         }
-
-        // update status
-        updateTokens(node, response);
-
-        // create or update current session
-        if (!session) {
-          // if no session identifier, do nothing
-          if (sessionId) {
-            await context.createSession({
-              sessionId,
-              previousId: response.id
-            });
-          }
-        } else {
-          // session exists, update it
-          await context.updateSession(sessionId, { previousId: response.id });
-        }
-
-        send(processOutputs(response.output, promptDesign, msg, response));
-        done();
       }
+
+      // update status
+      updateTokens(node, response);
+
+      // create or update current session
+      if (!session) {
+        // if no session identifier, do nothing
+        if (sessionId) {
+          await context.createSession({
+            sessionId,
+            previousId: response.id
+          });
+        }
+      } else {
+        // session exists, update it
+        await context.updateSession(sessionId, { previousId: response.id });
+      }
+
+      send(processOutputs(response.output, promptDesign, msg, response));
+      done();
     });
   }
 
